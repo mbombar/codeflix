ade4ad0b78689a3640e9fc8f63fcf8647f6dca50
Bombar Maxime <bombar@crans.org>
2020-01-27 16:42:44 +0100

    Update tests

--- codeflix/api/test.py
+++ codeflix/api/test.py
@@ -1,13 +1,20 @@
 import api
 
 def testusefulness():
-    assert(api.isuseful('1280'))
-    assert(not api.isuseful('409'))
-    assert(api.isuseful('412'))
-    contestslist = api.getcontestslist()
-    assert(len(contestslist) == 2)
-    contestsids = api.getcontestidslist(contestslist)
-    assert(len(contestsids) == 0)
+    try:
+        print('Testing isuseful')
+        assert(api.isuseful('1280'))
+        assert(not api.isuseful('409'))
+        assert(api.isuseful('412'))
+        print('Testing getcontestslist')
+        contestslist = api.getcontestslist()
+        assert(len(contestslist) == 2)
+        print('Testing getcontestidslist')
+        contestsids = api.getcontestidslist(contestslist)
+        assert(len(contestsids) == 0)
+        print('Testing passed !')
+    except AssertionError:
+        print('Error !')
 
 testusefulness()
 
