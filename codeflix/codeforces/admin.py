1e8275a96e7804c80b6add47128add6390f3acff
Bombar Maxime <bombar@crans.org>
2020-01-28 16:49:32 +0100

    [codeforces/admin] Allows to search into CodeforcesUser via a search bar in admin site

--- codeflix/codeforces/admin.py
+++ codeflix/codeforces/admin.py
@@ -1,6 +1,11 @@
 # Register your models here.
 
+from django.contrib import admin
 from DjangoTools import tools
 from .models import CodeforcesUser
 
 tools.autoregister(app_list=['codeforces'], ignored_models=[CodeforcesUser])
+
+@admin.register(CodeforcesUser)
+class CodeforcesUserAdmin(admin.ModelAdmin):
+    search_fields = ['handle', 'first_name', 'last_name']
