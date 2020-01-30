704f376365d97b30e02d7f0b38a2f3149d08feb0
gaubian <guillaume.aubian@gaubian.xyz>
2020-01-30 12:42:15 +0100

    Added recovering list of users and problems

--- codeflix/graph/generate_graph.py
+++ codeflix/graph/generate_graph.py
@@ -11,9 +11,12 @@ def add_edge(user, problem, solved, G):
     if solved or not G.has_edge(user,problem):
         G.add_edge(user, problem, weight=(0 if solved else 1))
 
-def add_contest(contestid, G):
+def add_contest(contestid, G, users_set, pbs_set):
     (solves, users, pbs) = api.solvedsubmissionsduringcontest(contestid)
+    for pb in pbs:
+        pbs_set.add(pb)
     for user in users:
+        users_set.add(user)
         for pb in pbs:
             add_edge(user, pb, False, G)
     for (user, pb) in solves:
@@ -21,11 +24,13 @@ def add_contest(contestid, G):
 
 def create_graph_from_ids(contestids):
     G = nx.Graph()
+    users = set()
+    problems = set()
     for contestid in contestids:
         # USED FOR TESTING
         print('NEW CONTEST ' + str(contestid))
-        add_contest(contestid, G)
-    return G
+        add_contest(contestid, G, users, problems)
+    return (G, users, problems)
 
 def create_graph():
     contestslist = api.getcontestslist()
--- codeflix/graph/test.py
+++ codeflix/graph/test.py
@@ -2,12 +2,12 @@ import generate_graph
 
 def testgraphcreation():
     print('Testing create_graph_from_ids')
-    G = generate_graph.create_graph_from_ids([1280])
+    (G, users, problems) = generate_graph.create_graph_from_ids([1280])
     assert(len(G.edges()) == 2958)
     print('Testing create_graph')
     # DEBUG PHASE : IT'S TOO SLOW
-    GG = generate_graph.create_graph()
-    print(len(GG.edges()))
+    (finalG, finalusers, finalproblems) = generate_graph.create_graph()
+    print(len(finalG.edges()))
     print('Testing passed !')
 
 testgraphcreation()
