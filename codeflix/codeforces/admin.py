6d7289a73328c76c9203f038bea3892922a9ec1b
Bombar Maxime <bombar@crans.org>
2020-02-01 06:30:24 +0100

    Allows to search for Problems by name in admin site

--- codeflix/codeforces/admin.py
+++ codeflix/codeforces/admin.py
@@ -2,10 +2,14 @@
 
 from django.contrib import admin
 from DjangoTools import tools
-from .models import CodeforcesUser
+from .models import CodeforcesUser, Problem
 
-tools.autoregister(app_list=['codeforces'], ignored_models=[CodeforcesUser])
+tools.autoregister(app_list=['codeforces'], ignored_models=[CodeforcesUser, Problem])
 
 @admin.register(CodeforcesUser)
 class CodeforcesUserAdmin(admin.ModelAdmin):
     search_fields = ['handle', 'first_name', 'last_name']
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    search_fields = ['name']
