278cbb17739af1d9a94bbff754f106850012507f
Bombar Maxime <bombar@crans.org>
2020-02-03 14:44:01 +0100

    [recommendation] Add test with real values for recommendation system

--- codeflix/recommendation/test.py
+++ codeflix/recommendation/test.py
@@ -1,6 +1,13 @@
 import recommendation as rec
 import networkx as nx
 
+import sys
+sys.path.append('../')
+from graph import generate_graph
+from api import api
+
+import argparse
+
 def createtestgraph():
     G = nx.Graph()
     G.add_edge('Alice', '1', weight=0)
@@ -38,4 +45,26 @@ def testeverything():
     testrecommendation()
     print('Testing passed !')
 
-testeverything()
+def realtest(user='LeCaRiBoU'):
+    print("Doing a test with real values")
+    print("Generating graph ...")
+    (G, users, problems) = generate_graph.create_graph(check=False, verbose=False)
+    print("Done !")
+    print("Computing recommendation")
+    sortedproblems = rec.recommendation(user, users, G)
+    assert(len(sortedproblems) == 3337)
+    problem = api.Problem.objects.filter(name=sortedproblems[0]).first()
+    print("We recommand {} to try and solve problem {}".format(user, problem))
+    print('Testing passed !')
+
+
+parser = argparse.ArgumentParser("Tests for codeflix recommendation system")
+
+parser.add_argument("-r", "--real", action="store_true", help="Do a test with real values")
+
+if __name__=="__main__":
+    args = parser.parse_args()
+    if args.real:
+        realtest()
+    else:
+        testeverything()
